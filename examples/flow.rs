331bedbebdcdd016a0f72ee7f54dc5cb8919c0bb
Todd Stellanova <tstellanova@users.noreply.github.com>
2020-07-16 10:51:46 -0700

    update external optical flow example

--- examples/flow.rs
+++ examples/flow.rs
@@ -6,6 +6,10 @@ LICENSE: BSD3 (see LICENSE file)
 #![no_main]
 #![no_std]
 
+//! This example exercises the PMW3901 optical flow sensor
+//! attached to the Pixhawk 4's external SPI port.
+//!
+
 use cortex_m_rt as rt;
 use rt::entry;
 
@@ -15,12 +19,11 @@ use embedded_hal::blocking::delay::DelayMs;
 use embedded_hal::digital::v2::OutputPin;
 use embedded_hal::digital::v2::ToggleableOutputPin;
 
-const LOOP_RATE_HZ: u16 = 200;
+const LOOP_RATE_HZ: u16 = 1;
 const LOOP_INTERVAL_MS: u16 = 1000 / LOOP_RATE_HZ;
 
-use pmw3901_ehal::{self, Builder};
+use pmw3901_ehal::{self, PMW3901};
 use pixhawk4_bsp::board::Board;
-use pixhawk4_bsp::peripherals::Spi5CS1;
 
 #[entry]
 fn main() -> ! {
@@ -36,34 +39,19 @@ fn main() -> ! {
     let _ = board.user_leds.1.set_low();
     let _ = board.user_leds.2.set_high();
 
-    let flow = pmw3901_ehal::PMW3901::new(
+    let mut flow = PMW3901::new(
         board.ext_spi.acquire(),
         board.ext_spi_csn.0
     );
+    flow.init(&mut board.delay_source).expect("could not init flow");
 
-    flow.init();
-
-    let mut loop_count: u32 = 0;
     loop {
-
-        rprintln!("baro [{}]: {} ", loop_count, sample.pressure);
-
-
-        if board.baro.is_some() {
-            if let Ok(sample) =
-                board.baro.as_mut().unwrap().get_second_order_sample(
-                    Oversampling::OS_2048,
-                    &mut board.delay_source,
-                )
-            {
-                rprintln!("baro [{}]: {} ", loop_count, sample.pressure);
-            }
+        if let Ok(sample) = flow.get_motion() {
+            rprintln!("flow {:?}",sample);
         }
-
         let _ = board.user_leds.0.toggle();
         let _ = board.user_leds.1.toggle();
+        let _ = board.user_leds.2.toggle();
         board.delay_source.delay_ms(loop_interval);
-
-        loop_count += 1;
     }
 }
